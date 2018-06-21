4b765af6903b5158a790759c768514612ec364ca
Maxim Shatalov <shatalov04@gmail.com>
2018-06-21 21:42:10 +0300

    lesson 5

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Article(props) {
-    const {article} = props;
-    const body = <section>{article.text}</section>;
+class Article extends Component {
+    // constructor(props) {
+    //     super(props);
+    //
+    //     this.state = {
+    //         isOpen: true
+    //     }
+    // }
 
-    return (
-        <div className="container" style={{color: 'red'}}>
-            <h2>{article.title}</h2>
-            {body}
-            <h3>
-                Creation date: {(new Date(article.date)).toDateString()}
-            </h3>
+    state = {
+        isOpen: false
+    };
 
-        </div>
-    )
+    render() {
+        const {article} = this.props;
+        const body = this.state.isOpen && <section>{article.text}</section>;
+
+        return (
+            <div className="container" style={{color: 'red'}}>
+                <h2>
+                    {article.title}
+                    <button onClick={this.handleClick}>
+                        {this.state.isOpen ? 'close' : 'open'}
+                    </button>
+                </h2>
+                {body}
+                <h3>
+                    Creation date: {(new Date(article.date)).toDateString()}
+                </h3>
+
+            </div>
+        )
+    }
+
+    handleClick = () => {
+        console.log('---', 'clicked');
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
 }
 
 export default Article
